84f2196aa8996edd435687ad01042c2fb1e7b586
rezafaraji <faraji.reza@outlook.com>
2024-08-02 05:51:17 -0400

    Added footer

--- composeApp/src/commonMain/kotlin/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/home/presentation/HomeScreen.kt
@@ -1,20 +1,28 @@
+@file:OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+
 package home.presentation
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import component.DescriptionComponent
 import component.ExperienceItem
 import component.TitleComponent
@@ -23,6 +31,7 @@ import core.domain.HomeData
 import myportfolio.composeapp.generated.resources.Res
 import myportfolio.composeapp.generated.resources.about_me
 import myportfolio.composeapp.generated.resources.experiences
+import myportfolio.composeapp.generated.resources.how_is_it_made
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -89,6 +98,21 @@ fun HomeScreenContent(
         }
         item {
             Spacer(modifier = Modifier.height(24.dp))
+        }
+        item {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.surfaceContainer)
+                    .padding(horizontal = 20.dp, vertical = 16.dp),
+                contentAlignment = Alignment.Center) {
+                Text(
+                    text = stringResource(Res.string.how_is_it_made),
+                    style = MaterialTheme.typography.labelSmall.copy(
+                        fontSize = 12.sp
+                    )
+                )
+            }
         }
     }
 }
